3bd5349a48c1f2202a4b81e3eaa72d935655fa6a
Roilan <roilan14@gmail.com>
2016-05-17 07:04:28 -0400

    category details in single category view

--- App/Utils/index.js
+++ App/Utils/index.js
@@ -24,3 +24,21 @@ export function createListData(dataObj) {
 
   return dataBlob;
 }
+
+export const date = (() => {
+  const today = new Date();
+  const monthNames = {
+    1: 'January',  2: 'Feburary',  3: 'March',  4: 'April',
+    5: 'May',  6: 'June',  7: 'July',  8: 'August',
+    9: 'September',  10: 'October',  11: 'November',  12: 'December'
+  };
+  const month = today.getMonth() + 1;
+
+
+  return {
+    day: today.getDate(),
+    month,
+    monthName: monthNames[month],
+    year: today.getFullYear()
+  }
+})();
--- app/containers/SelectedCategory.js
+++ app/containers/SelectedCategory.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import { connect } from 'react-redux';
 import List from '../components/common/List';
-import { colorOfNumber } from '../utils';
-import { borderColors } from '../utils/styles';
+import { colorOfNumber, date } from '../utils';
+import { borderColors, colors } from '../utils/styles';
 
 class TransactionView extends Component {
   constructor() {
@@ -34,15 +34,63 @@ class TransactionView extends Component {
     const { category } = this.props;
 
     return (
-      <List
-        dataSource={category}
-        renderRow={this.renderRow}
-      />
+      <View style={styles.container}>
+        <View style={styles.categoryDetailsRow}>
+          <View style={styles.categoryDetails}>
+            <Text style={styles.categoryDetailsText}>
+              {`Funded in ${date.monthName}`.toUpperCase()}
+            </Text>
+
+            <Text style={[styles.categoryDetailsText, styles.categoryDetailsAmount]}>
+              $100.00
+            </Text>
+          </View>
+
+          <View></View>
+
+          <View style={styles.categoryDetails}>
+            <Text style={styles.categoryDetailsText}>
+              {`Spent In ${date.monthName}`.toUpperCase()}
+            </Text>
+
+            <Text style={[styles.categoryDetailsText, styles.categoryDetailsAmount]}>
+              $0.00
+            </Text>
+          </View>
+        </View>
+
+        <List
+          dataSource={category}
+          renderRow={this.renderRow}
+        />
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  categoryDetailsRow: {
+    backgroundColor: '#0B1D26',
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  },
+  categoryDetails: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 10,
+    paddingBottom: 10
+  },
+  categoryDetailsText: {
+    color: colors.white,
+    fontSize: 14,
+    fontWeight: '400',
+  },
+  categoryDetailsAmount: {
+    paddingTop: 2
+  },
   sectionRow: {
     justifyContent: 'space-between',
     flexDirection: 'row',
